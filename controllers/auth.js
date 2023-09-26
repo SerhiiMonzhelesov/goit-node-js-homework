2411ce036188698b5e18cf70104b3349635c4b07
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-09-26 22:19:16 +0300

    create saltPass ctrlLogin token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,53 @@
+require("dotenv").config();
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const { User } = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
+const { SECRET_KEY } = process.env;
+
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
     throw HttpError(409, "Email in use");
   }
 
-  const newUser = await User.create(req.body);
+  const hashPassword = await bcryptjs.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
   res.status(201).json({
     email: newUser.email,
     name: newUser.name,
   });
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw HttpError(401, "email or password is wrong");
+  }
+  const passwordCompare = await bcryptjs.compare(password, user.password);
+
+  if (!passwordCompare) {
+    throw HttpError(401, "email or password is wrong");
+  }
+  const payload = { id: user._id };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+  res.json({
+    token,
+    user: {
+      email,
+      // subscription: "starter",
+    },
+  });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
 };
