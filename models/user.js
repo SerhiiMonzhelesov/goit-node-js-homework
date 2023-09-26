5c374378883d2abdd1182f4d7f41f66c0047a2fa
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-09-26 10:32:33 +0300

    create userModel userSchema joiSchema for register

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { handleMongooseError } = require("../helpers");
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      unique: true,
+      required: [true, "Email is required"],
+    },
+    password: {
+      type: String,
+      minLength: 6,
+      required: [true, "Set password for user"],
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const schemas = {
+  registerSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  schemas,
+};
